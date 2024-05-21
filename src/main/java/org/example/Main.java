ed78d3720156caa66d43fd8a08744394bd7072ce
geovannad <geovanna.diniz@germinare.org.br>
2024-05-21 09:46:40 -0300

    adicionando for

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        for (int i = 0; i < 10; i++){
+            System.out.println("Essa é a " + i + "° que estou passando pelo looping");
+        }
     }
 }
